62d7ab29b9e64f95948bc82dde65dbb8ff0d7627
fighter_lee <fighterlee1@gmail.com>
2020-06-16 20:01:08 +0800

    jni

--- code/ndk/lsn_2/lsn_2_example/lsn_2_example/lsn_2_example.cpp
+++ code/ndk/lsn_2/lsn_2_example/lsn_2_example/lsn_2_example.cpp
@@ -1,9 +1,17 @@
 ﻿// lsn_2_example.cpp: 定义应用程序的入口点。
 //
 
+#include <jni.h>
 #include "lsn_2_example.h"
 
 using namespace std;
+
+extern "C"
+JNIEXPORT void JNICALL Java_com_fighter_ndkproject_JniTool_stringFromJNI(
+JNIEnv *env,jobject instance,jint i,jstring j) {
+	const char* str = env->GetStringUTFChars(j,0);
+	printf("%d,%s",i,str);
+}
 
 int main()
 {
